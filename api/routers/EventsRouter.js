874138e8c0f550e6f43752986a5e7a79ee639436
IliaGamazin <143553365+IliaGamazin@users.noreply.github.com>
2025-12-06 18:32:44 +0200

    togglebale tasks (#21)

--- api/routers/EventsRouter.js
+++ api/routers/EventsRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { authenticate } from "../middleware/AuthMiddleware.js";
 import {
     delete_event, get_event,
-    get_events, new_event, update_event
+    get_events, new_event, update_event, toggle_task
 } from "../controllers/EventsController.js";
 
 router.use(authenticate);
@@ -12,6 +12,7 @@ router.use(authenticate);
 router.get("/", get_events);
 router.post("/", new_event);
 router.get("/:event_id", get_event);
+router.post("/:event_id/toggle_task", toggle_task);
 router.patch("/:event_id", update_event);
 router.delete("/:event_id", delete_event);
 
